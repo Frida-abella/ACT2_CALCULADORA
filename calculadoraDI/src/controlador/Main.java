8e5eba6b4b3915ab3640a7f7e5192c4fb12bfd22
DavidMatias94 <78035910+DavidMatias94@users.noreply.github.com>
2021-11-28 00:24:30 +0100

    Add files via upload
    Calculadora básica creada con Java Swing

---
+++ calculadoraDI/src/controlador/Main.java
@@ -0,0 +1,16 @@
+package controlador;
+
+import calculadora.VentanaPrincipalGUI;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//instancio un objeto de la clase VentanaPrincipalGUI, como está en otro paquete la tengo que importar
+		
+		VentanaPrincipalGUI miVentana = new VentanaPrincipalGUI();
+	
+
+	}
+
+}
